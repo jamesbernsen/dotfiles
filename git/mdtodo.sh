1e1ecea87a3c102339d2c79d7d25330b75001470
James Bernsen <jrbtex@gmail.com>
2017-06-29 14:41:25 -0400

    Add script to parse TODOs and insert in markdown
    - Add mdtodo.sh script that uses the leasot tool to parse TODO comments
    in code and write a markdown table (describing the TODOs) into an existing
    markdown file.
    - Install mdtodo.sh in ~/bin

---
+++ git/mdtodo.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+MDFILE=$1; shift
+HEADER='### TODOs'
+TRAILER='###### TODOs parsed by [leasot](https://github.com/pgilad/leasot)'
+
+# Credit: https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed
+ESCAPE_LITERAL_RGX='s/[^^]/[&]/g; s/\^/\\^/g'
+
+HEADER_RGX=$(sed "$ESCAPE_LITERAL_RGX" <<<"$HEADER")
+TRAILER_RGX=$(sed "$ESCAPE_LITERAL_RGX" <<<"$TRAILER")
+
+MYTMPDIR=$(mktemp --directory --tmpdir pid$$.XXXXXXXXXX)
+TEMPFILE=$(mktemp --tmpdir=$MYTMPDIR)
+
+# In the markdown file, capture any text after the trailer pattern to a file.
+sed "0,\\|$TRAILER_RGX|d" $MDFILE > $TEMPFILE
+
+# In the markdown file, delete any text after the header pattern.
+sed -i "\\|^$HEADER_RGX\$|,\$d" $MDFILE
+
+# Append the table and trailer to the markdown file.
+leasot --reporter markdown $@ >> $MDFILE
+echo "$TRAILER" >> $MDFILE
+
+# Restore the previous markdown "tail" to the file.
+cat $TEMPFILE >> $MDFILE
+
+rm -f $TEMPFILE
+rmdir --ignore-fail-on-non-empty $MYTMPDIR
+
