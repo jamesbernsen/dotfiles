a224e1ec7abe12a449f6f6de6da48cec2b915052
James Bernsen <jrbtex@gmail.com>
2017-06-29 23:21:50 -0400

    Add command-line argument parsing and usage help

--- git/mdtodo.sh
+++ git/mdtodo.sh
@@ -1,12 +1,71 @@
 #! /usr/bin/env bash
 
+#############################################################################
+show_usage() {
+  read -r -d '' usage << USAGE_END
+
+Usage: mdtodo [OPTION]... <SOURCE-FILE>...
+Parses TODOs from code using leasot and inserts them into a Markdown file
+
+Options:
+  -h,?           display this usage information
+  -o [filename]  the Markdown file to update with a TODO table
+  -t [filetype]  force the filetype to parse. Useful if the source-file's
+                 extension is unusual, changed, or removed
+
+Examples:
+  mdtodo -o README.md script.sh
+  mdtodo -o TODOs.md -t .c foo.C bar.C
+  mdtodo -o README.md -t .py my_executable_python_script
+
+USAGE_END
+
+  echo
+  echo "$usage"
+  echo
+}
+
+
+#############################################################################
+
 # Check for prerequisites
 declare -a prereqs=("leasot" "sed")
 for prog in "${prereqs[@]}" ; do
-  command -v $prog > /dev/null 2>&1 || { echo >&2 "Program '$prog' required but not found. Aborting."; exit 1; }
+  command -v $prog > /dev/null 2>&1 || { echo >&2 "Program '$prog' required but not found."; exit 1; }
 done
 
-MDFILE=$1; shift
+# Get command-line options
+while getopts "h?o:t:" opt; do
+  case "$opt" in
+    h|\?)
+      show_usage
+      exit 0
+      ;;
+    o)
+      mdfile=$OPTARG
+      ;;
+    t)
+      filetype_arg="--filetype $OPTARG"
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+EX_USAGE=64
+
+if [[ ! -r "$mdfile" || ! -w "$mdfile" ]] ; then
+  echo >&2 "The -o option is required, and its filename must be readable and writable."
+  show_usage
+  exit $EX_USAGE
+fi
+
+if [[ "$#" -lt "1" ]] ; then
+  echo >&2 "One or more source filenames are required."
+  show_usage
+  exit $EX_USAGE
+fi
+
 HEADER='### TODOs'
 TRAILER='###### TODOs parsed by [leasot](https://github.com/pgilad/leasot)'
 
@@ -16,22 +75,22 @@ ESCAPE_LITERAL_RGX='s/[^^]/[&]/g; s/\^/\\^/g'
 HEADER_RGX=$(sed "$ESCAPE_LITERAL_RGX" <<<"$HEADER")
 TRAILER_RGX=$(sed "$ESCAPE_LITERAL_RGX" <<<"$TRAILER")
 
-MYTMPDIR=$(mktemp --directory --tmpdir pid$$.XXXXXXXXXX)
-TEMPFILE=$(mktemp --tmpdir=$MYTMPDIR)
+mytmpdir=$(mktemp --directory --tmpdir pid$$.XXXXXXXXXX)
+tempfile=$(mktemp --tmpdir=$mytmpdir)
 
 # In the markdown file, capture any text after the trailer pattern to a file.
-sed "0,\\|$TRAILER_RGX|d" $MDFILE > $TEMPFILE
+sed "0,\\|$TRAILER_RGX|d" $mdfile > $tempfile
 
 # In the markdown file, delete any text after the header pattern.
-sed -i "\\|^$HEADER_RGX\$|,\$d" $MDFILE
+sed -i "\\|^$HEADER_RGX\$|,\$d" $mdfile
 
 # Append the table and trailer to the markdown file.
-leasot --reporter markdown $@ >> $MDFILE
-echo "$TRAILER" >> $MDFILE
+leasot --reporter markdown $filetype_arg $@ >> $mdfile
+echo "$TRAILER" >> $mdfile
 
 # Restore the previous markdown "tail" to the file.
-cat $TEMPFILE >> $MDFILE
+cat $tempfile >> $mdfile
 
-rm -f $TEMPFILE
-rmdir --ignore-fail-on-non-empty $MYTMPDIR
+rm -f $tempfile
+rmdir --ignore-fail-on-non-empty $mytmpdir
 
