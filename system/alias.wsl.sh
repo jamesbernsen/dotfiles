55fe53b82b1e1cc0fcb8cd9c17f1b950e614d1d8
James Bernsen <jrbtex@gmail.com>
2017-10-20 13:50:30 -0400

    Add convenience alias for cmd.exe /c in WSL

--- system/alias.wsl.sh
+++ system/alias.wsl.sh
@@ -1,5 +1,7 @@
 # Alias to run Windows cmd.exe from WSL
 # Useful, for example, to get Windows environment vars:
-# cmd /c SET WINDIR
+# cmd /c SET
 alias cmd="/mnt/c/Windows/System32/cmd.exe"
+# Because typing "/c" in a Linux shell feels wrong:
+alias cmdc="/mnt/c/Windows/System32/cmd.exe /c"
 
