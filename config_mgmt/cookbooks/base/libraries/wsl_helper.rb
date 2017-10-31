7242d5e4dd38871e2fb1ef00ed23ef61a2f21ef7
James Bernsen <jrbtex@gmail.com>
2017-10-31 11:49:06 -0400

    Add helper function to get choco package versions

--- config_mgmt/cookbooks/base/libraries/wsl_helper.rb
+++ config_mgmt/cookbooks/base/libraries/wsl_helper.rb
@@ -1,30 +1,38 @@
 # Helpers for Windows Subsystem for Linux (WSL)
 
 module BaseLib
-	module WSL
 
-		include Chef::Mixin::ShellOut
+  module WSL
+    include Chef::Mixin::ShellOut
 
-		def is_WSL?
-			return (node['kernel']['name'].include? "Linux" and node['os_version'].include? "Microsoft")
-		end
+    def is_WSL?
+      return (node['kernel']['name'].include? "Linux" and node['os_version'].include? "Microsoft")
+    end
 
-		def choco_pkg_local_install?(pkg_name)
-			cmd = shell_out!('choco.exe list --localonly -r', {:environment => { "PATH" => "/mnt/c/ProgramData/chocolatey/bin" }, :returns => [0]})
-			return (cmd.stdout =~ /#{RegExp.escape(pkg_name)}/)
-		end
+    def choco_pkg_local_install?(pkg_name)
+      cmd = shell_out!("choco.exe list --localonly -r #{pkg_name}", {:environment => { "PATH" => "/mnt/c/ProgramData/chocolatey/bin" }, :returns => [0]})
+      return (cmd.stdout =~ /#{RegExp.escape(pkg_name)}/)
+    end
 
-		def choco_pkgs_locally_installed
-			cmd = shell_out!('choco.exe list --localonly -r', {:environment => { "PATH" => "/mnt/c/ProgramData/chocolatey/bin" }, :returns => [0]})
-			list_text = cmd.stdout
-			list_text.gsub(/\r\n?/, "\n")
-			app_lines = list_text.split("\n")
-			app_ver = Hash.new
-			app_lines.each do |app_line|
-				app_ver[ app_line.split("|")[0] ] = app_line.split("|")[1]
-			end
-			return app_ver
-		end
+    def choco_pkgs_locally_installed
+      cmd = shell_out!('choco.exe list --localonly -r', {:environment => { "PATH" => "/mnt/c/ProgramData/chocolatey/bin" }, :returns => [0]})
+      list_text = cmd.stdout
+      list_text.gsub(/\r\n?/, "\n")
+      pkg_lines = list_text.split("\n")
+      pkg_ver = Hash.new
+      pkg_lines.each do |pkg_line|
+        pkg_ver[ pkg_line.split("|")[0] ] = pkg_line.split("|")[1]
+      end
+      return pkg_ver
+    end
 
-	end
+    def choco_pkg_local_version(pkg_name)
+      cmd = shell_out!("choco.exe list --localonly -r #{pkg_name}", {:environment => { "PATH" => "/mnt/c/ProgramData/chocolatey/bin" }, :returns => [0]})
+      pkg_line = cmd.stdout
+      pkg_line = pkg_line.gsub(/\r|\n/, "")
+      ver = pkg_line.split("|")[1]
+      return ver
+    end
+  end
+
 end
